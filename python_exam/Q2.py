21f3715e30f37f18fdd540f77e141718ceadf859
Akhil Pratap Singh <iamakhil150610@gmail.com>
2022-11-08 12:36:20 +0530

    Update Q2.py

--- python_exam/Q2.py
+++ python_exam/Q2.py
@@ -5,7 +5,7 @@ However, DO NOT CHANGE THE TEMPLATE CHANGE THE TEMPLATE OF THE FUNCTIONS PROVIDE
 """
 
 
-def bidirectional_dij(source: int, destination: int, graph_object) -> int:
+def bidirectional_dij(G, S, T) -> int:
     """
     Bi-directional Dijkstra's algorithm.
 
@@ -20,6 +20,29 @@ def bidirectional_dij(source: int, destination: int, graph_object) -> int:
     Warnings:
         If the destination is not reachable, function returns -1
     """
+    start = [Queue(S, 0.0)]                       # Creating initial start node using HeapQ and setting its value to 0.0
+    goal = set()
+    pred = dict()                                                         # Dictionary to store visited nodes in a graph
+    dist = dict()                                                     # Dictionary to store distance from point to point
+    pred[S] = None
+    dist[S] = 0.0
+    while start:
+        C = hq.heappop(start).v                   # Pop the smallest item off the heap, maintaining the heap invariant.
+        if C == T:
+            return traversal(T, pred)
+        goal.add(C)
+        for pointer in G[C]:
+            if pointer in goal:
+                continue
+            dist_temp = dist[C] + G[C][pointer]['weight']
+            if pointer not in dist or dist[pointer] > dist_temp:                          #Checking vertex with low cost
+                dist[pointer] = dist_temp
+                pred[pointer] = C
+                hq.heappush(start, Queue(pointer, dist[C] + G[C][pointer]['weight']))          # Adding vertex to queue
+    return []
+    
+    
+    
     shortest_path_distance = -1
 
     try:
